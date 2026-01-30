3a0e2aad7cdf452e1750284f03230baaa3c353e3
tejeshwar-01 <Tejeshwarreddy272@gmail.com>
2026-01-30 11:42:52 +0530

    Add files via upload

---
+++ week3/SecondHighest.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class SecondHighest
+{
+   public static void main(String args[])
+   {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i< n;i++)
+        {
+            list.add(sc.nextInt());
+        }
+
+        int SecondHighest = list.stream().distinct()
+                                .sorted(Comparator.reverseOrder())
+                                .skip(1)
+                                .findFirst()
+                                .orElse(-1);
+
+        System.out.println(SecondHighest);
+
+        sc.close();
+   }
+}
